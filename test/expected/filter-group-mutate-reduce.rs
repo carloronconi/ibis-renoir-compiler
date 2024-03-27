08c0695dd4b8c831e056277a70969c5b9674c49a
carloronconi <carlo.ronconi@mail.polimi.it>
2024-03-27 19:36:17 +0100

    wip: update passing tests expected files

--- test/expected/filter-group-mutate-reduce.rs
+++ test/expected/filter-group-mutate-reduce.rs
@@ -1,19 +1,34 @@
 use noir_compute::prelude::*;
 use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq)]
-struct Cols_table0 {
+struct Struct_var_0 {
     int1: i64,
     string1: String,
 }
+#[derive(Clone, Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq)]
+struct Struct_var_1 {
+    int1: i64,
+    string1: String,
+    mul: i64,
+}
+#[derive(Clone, Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq)]
+struct Struct_var_2 {
+    agg: i64,
+}
 
 fn logic(ctx: &StreamContext) {
-    let table0 = ctx
-        .stream_csv::<Cols_table0>("/home/carlo/Projects/ibis-quickstart/data/int-1-string-1.csv");
-    table0
-        .filter(|x| x.string1 == "unduetre")
+    let var_0 = ctx
+        .stream_csv::<Struct_var_0>("/home/carlo/Projects/ibis-quickstart/data/int-1-string-1.csv");
+    var_0
+        .filter(|x| x.int1 > 200)
+        .map(|x| Struct_var_1 {
+            int1: x.int1,
+            string1: x.string1,
+            mul: x.int1 * 20,
+        })
         .group_by(|x| x.string1.clone())
-        .map(|(_, x)| x.int1 * 20)
-        .reduce(|a, b| *a = (*a).max(b)))
+        .reduce(|a, b| a.mul = a.mul + b.mul)
+        .map(|(_, x)| Struct_var_2 { agg: x.mul })
         .for_each(|x| println!("{x:?}"));
 }
 
