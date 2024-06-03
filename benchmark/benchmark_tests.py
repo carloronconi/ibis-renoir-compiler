39dda06d4fc7256796f594bbd39e7efbd5dcbd89
carloronconi <carlo.ronconi@mail.polimi.it>
2024-06-03 15:19:33 +0200

    feat: add memory benchmarking with time shell tool

---
+++ benchmark/benchmark_tests.py
@@ -0,0 +1,16 @@
+import subprocess
+import unittest
+import test
+
+def main():
+    test_classes = test.TestCompiler.__subclasses__()
+    test_suites = [unittest.TestLoader().loadTestsFromTestCase(c) for c in test_classes]
+    test_cases = [t.id() for s in test_suites for t in s]
+
+    for t in test_cases:
+        name = t.split(".")[-1]
+        subprocess.run(f"command time -a -o \"log/memo_log.csv\" -f \"{name}\",\"%e\",\"%M\",\"%U\",\"%S\",\"%x\" python -m unittest {t}", shell=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+from .test_base import TestCompiler
+from .test_operators import *
+from .test_nexmark import *
