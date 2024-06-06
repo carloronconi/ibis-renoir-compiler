46462c52262edc5aacfcf912174e20321f309d9f
carloronconi <carlo.ronconi@mail.polimi.it>
2024-06-06 11:41:35 +0200

    chore: ignore extended datasets and comment hyperfine  script

--- benchmark/hyperfine_benchmark.sh
+++ benchmark/hyperfine_benchmark.sh
@@ -3,7 +3,12 @@
 # add the backends you want to compare renoir against
 backends_compare_against=("duckdb")
 # backends_compare_against=("duckdb" "polars")
-file_size="100000000"
+
+# if you want to run the benchmark on a different dataset size, change the size_suffix, otherwise leave ""
+# the git repository only contains the base non-suffixed files, so in case you use this variable you
+# need to generate the extended files first.
+# For nexmark, `cd data/nexmark_data_gen && cargo run -- 100000000`
+size_suffix="_100000000"
 
 source .venv/bin/activate
 # change grep to filter the tests you want to run
@@ -11,8 +16,8 @@ python -m benchmark.discover_tests | grep "nexmark_query_2" | while IFS= read -r
     trim=${name##*.}
     for backend in "${backends_compare_against[@]}"; do
         hyperfine --warmup 5 \
-        "python ../ibis-quickstart $name --backend renoir --path_suffix _$file_size" \
-        "python ../ibis-quickstart $name --backend $backend --path_suffix _$file_size" \
-        --export-json log/hyperfine_${backend}_${trim}_${file_size}.json
+        "python ../ibis-quickstart $name --backend renoir --path_suffix $size_suffix" \
+        "python ../ibis-quickstart $name --backend $backend --path_suffix $size_suffix" \
+        --export-json log/hyperfine_${backend}_${trim}${size_suffix}.json
     done
 done
