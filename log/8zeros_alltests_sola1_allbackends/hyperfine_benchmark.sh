9d810c8ca19c3a48c04a18b8c9027331855c6ce2
carloronconi <carlo.ronconi@mail.polimi.it>
2024-06-12 11:53:15 +0200

    chore: run benchmarks on sola1

---
+++ log/8zeros_alltests_sola1_allbackends/hyperfine_benchmark.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# add the backends you want to compare renoir against
+backends=("renoir" "duckdb" "polars" "flink")
+# backends=("duckdb" "polars")
+
+# if you want to run the benchmark on a different dataset size, change the size_suffix, otherwise leave ""
+# the git repository only contains the base non-suffixed files, so in case you use this variable you
+# need to generate the extended files first.
+# For nexmark, `cd data/nexmark_data_gen && cargo run -- 100000000`
+size_suffix="_100000000"
+
+source .venv/bin/activate
+# change grep to filter the tests you want to run
+mkdir -p log/$1
+python3 -m benchmark.discover_tests | while IFS= read -r name; do
+    trim=${name##*.}
+    for backend in "${backends[@]}"; do
+        hyperfine --warmup 1 \
+        "python3 ../ibis-renoir-compiler $name --backend $backend --path_suffix $size_suffix" \
+        --export-json log/$1/hyperfine_${trim}${size_suffix}_${backend}.json
+        printf "\n"
+        # copy every time we run a test so if we quit before finish all tests we still have both
+        cp log/codegen_log.csv log/$1/codegen_log.csv
+        cp benchmark/hyperfine_benchmark.sh log/$1/hyperfine_benchmark.sh
+    done
+done
