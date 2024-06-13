696df45121027d01ffaadac58cf0f151a2f2dacb
carloronconi <carlo.ronconi@mail.polimi.it>
2024-06-13 13:59:01 +0200

    feat: use mimalloc allocator in renoir template

--- noir_template/main_top.rs
+++ noir_template/main_top.rs
@@ -2,3 +2,7 @@ use renoir::prelude::*;
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::cmp::max;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
