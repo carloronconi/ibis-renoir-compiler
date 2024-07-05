2bd9e86f89eff92ede5b1303aae73ed699ec7b63
carloronconi <carlo.ronconi@mail.polimi.it>
2024-07-05 10:24:49 +0200

    chore: run benches on sola1

---
+++ log/internal/1M_all_sola1_6backs/internal_benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# if you want to run the benchmark on a different dataset size, change the path_suffix, otherwise leave ""
+# the git repository only contains the base non-suffixed files, so in case you use this variable you
+# need to generate the extended files first.
+# for nexmark, `cd data/nexmark_data_gen && cargo run -- 10000000`
+# for operators, `cd data/operators_data_gen && cargo run -- 10000000` 
+
+source .venv3.11/bin/activate
+
+python -m benchmark.internal.internal_benchmark \
+    --test_patterns TestNullable TestNexmark \
+    --runs 5 \
+    --warmup 1 \
+    --table_origin csv cached \
+    --dir internal/$1 \
+    --backends duckdb polars flink postgres risingwave renoir \
+    --path_suffix _1000000 \
+&& cp benchmark/internal/internal_benchmark.sh log/internal/$1/internal_benchmark.sh
+
