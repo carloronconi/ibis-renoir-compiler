635903bc7dae71ea6c7d57f04d0fff05704628fb
carloronconi <carlo.ronconi@mail.polimi.it>
2024-07-12 16:48:06 +0200

    wip: fix expected preload but not generator!

--- noir_template/preload_evcxr.rs
+++ noir_template/preload_evcxr.rs
@@ -7,8 +7,17 @@ use renoir::operator::cache::StreamCache;
 struct Structdatanullableopintsstrings {int1: Option<i64>,string1: Option<String>,int4: Option<i64>,}
 #[derive(Clone, Debug, Serialize, Deserialize, PartialOrd, PartialEq, Default)]
 struct Structdatanullableopmanyints {int1: Option<i64>,int2: Option<i64>,int3: Option<i64>,}
-
+fn cache() -> (StreamCache<Structdatanullableopintsstrings>,
+StreamCache<Structdatanullableopmanyints>) {
 let ctx = StreamContext::new_local();
-let (datanullableopintsstrings, _): (StreamCache<Structdatanullableopintsstrings>, _) = ctx.stream_csv::<Structdatanullableopintsstrings>("../data/nullable_op/ints_strings.csv").batch_mode(BatchMode::fixed(16000)).cache();
-let (datanullableopmanyints, _): (StreamCache<Structdatanullableopmanyints>, _) = ctx.stream_csv::<Structdatanullableopmanyints>("../data/nullable_op/many_ints.csv").batch_mode(BatchMode::fixed(16000)).cache();
+let (datanullableopintsstrings, a) = ctx.stream_csv::<Structdatanullableopintsstrings>("/home/carlo/Projects/ibis-renoir-compiler/data/nullable_op/ints_strings.csv").batch_mode(BatchMode::fixed(16000)).cache();
+a.for_each(|x| {
+    std::hint::black_box(x);
+});
+let (datanullableopmanyints, b) = ctx.stream_csv::<Structdatanullableopmanyints>("/home/carlo/Projects/ibis-renoir-compiler/data/nullable_op/many_ints.csv").batch_mode(BatchMode::fixed(16000)).cache();
+b.for_each(|x| {
+    std::hint::black_box(x);
+});
 ctx.execute_blocking();
+return (datanullableopintsstrings, datanullableopmanyints);
+}
