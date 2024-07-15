14ea97c0a87c6a29586e0089028b20a50e421f0e
carloronconi <carlo.ronconi@mail.polimi.it>
2024-07-15 10:13:27 +0200

    fix: change dependency order for evcxr

--- benchmark/start_evcxr.sh
+++ benchmark/start_evcxr.sh
@@ -2,4 +2,5 @@
 rustup component add rust-src
 cargo install --locked evcxr_repl
 # important! only way to cache dependencies
+# also start evcxr and pass the command :cache 5000
 export EVCXR_TMPDIR="/home/$USER/evcxr_temp/"
