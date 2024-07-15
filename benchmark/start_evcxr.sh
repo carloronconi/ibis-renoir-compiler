4cd4c2eb49df6545574060484d30f92f4f2302ff
carloronconi <carlo.ronconi@mail.polimi.it>
2024-07-15 12:06:59 +0200

    nit: update comment

--- benchmark/start_evcxr.sh
+++ benchmark/start_evcxr.sh
@@ -5,5 +5,4 @@ cargo install --locked evcxr_repl
 export EVCXR_TMPDIR="/home/$USER/evcxr_temp/"
 # also start evcxr and pass the command :cache 5000 and then manually copy and paste init.evcxr into evcxr
 # then check :last_compile_dir to see it's the one set with the env var.
-# sometimes it doesn't run there, probably when there's a evcxr instance already running that hasn't been
-# killed correctly
+# careful: when a shell is closed, you need to set EVCXR_TMPDIR again.
