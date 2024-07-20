0d9b6354d3a423910eb0c963419b873cc38a2b61
carloronconi <carlo.ronconi@mail.polimi.it>
2024-07-20 17:40:28 +0200

    chore: add test query to scenario 1

--- test/test_scenarios.py
+++ test/test_scenarios.py
@@ -9,27 +9,33 @@ class TestScenarios(TestNullableOperators):
         table = self.tables["ints_strings"]
         self.query = (table
                       .filter(table.int4.notnull()))
-
         self.complete_test_tasks("ints_strings")
 
     def test_scenarios_preprocess_2_fillna(self):
         self.query = (self.tables["ints_strings"]
                       .int4.fillna(0))
-
         self.complete_test_tasks("ints_strings")
 
     def test_scenarios_preprocess_3_filter(self):
+        # filter with two conditions including one requiring computation
         self.query = (self.tables["ints_strings"]
                       .filter((_.int1 % 2 == 0) & _.int4.notnull())
                       .select("int1"))
-
         self.complete_test_tasks("ints_strings")
 
-    def test_scenarios_preprocess_4_group_agg(self):
+    def test_scenarios_preprocess_4_group_sum(self):
+        # filter out many, group and aggregate with one accumulator
         self.query = (self.tables["ints_strings"]
                       .filter(_.string1.contains("a"))
                       .group_by("string1")
                       .aggregate(int1_agg=_["int1"].sum())
                       .select(["int1_agg"]))
+        self.complete_test_tasks("ints_strings")
         
+    def test_scenarios_preprocess_5_group_mean(self):
+        # without filtering, group and aggregate with two accumulators
+        self.query = (self.tables["ints_strings"]
+                      .group_by("string1")
+                      .aggregate(int1_agg=_["int1"].mean())
+                      .select(["int1_agg"]))
         self.complete_test_tasks("ints_strings")
