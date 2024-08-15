711ea51cca79cb9fcc43161813a9f6544ab21e46
carloronconi <carlo.ronconi@mail.polimi.it>
2024-08-15 16:38:15 +0200

    feat: complete flink prototype

--- benchmark/internal/consumer.py
+++ benchmark/internal/consumer.py
@@ -11,18 +11,25 @@ class Cons:
         }
         self.consumer = Consumer(config)
     
-    def consume(self, topic, start_attempts=6, timeout=10):
+    def consume(self, topic, start_attempts=6, timeout=10, max_messages=None):
+        print(f"Consuming messages from topic {topic}")
         self.consumer.subscribe([topic])
         messages = []
         while True:
             msg = self.consumer.poll(timeout)
             if msg is None:
-                print("No messages in consumer after {timeout} seconds at attempt {start_attempts}")
+                print(f"No messages in consumer after {timeout} seconds at attempt {start_attempts}")
                 if start_attempts == 0:
                     break
                 start_attempts -= 1
                 continue
+            if msg.error():
+                print(f"Consumer error: {msg.error()}")
+                break
             start_attempts = 0
             messages.append(msg.value().decode('utf-8'))
+            if max_messages and len(messages) >= max_messages:
+                # don't close in this case
+                return messages
         self.consumer.close()
         return messages
--- views_spark_proto.py
+++ views_spark_proto.py
@@ -5,10 +5,37 @@ from benchmark.internal.consumer import Cons
 from benchmark.internal.spark_connector import create_stream_query
 import random
 import string
+# import confluent_kafka.admin, pprint
 
 
 def rand_string(prefix="", len=16):
     return prefix + "".join(random.choices(string.ascii_letters, k=len))
+
+
+# this breaks it! this way even when consuming the producer topic directly, kafka is stuck
+# fixed only by restarting kafka
+# def init_topics(new_topics):
+#     admin = confluent_kafka.admin.AdminClient({'bootstrap.servers': 'localhost:9092'})
+#     futs = admin.delete_topics(list(admin.list_topics().topics.keys()))
+#     for topic, future in futs.items():
+#         try:
+#             future.result()
+#             print(f"Deleted topic {topic}")
+#         except Exception as e:
+#             print(f"Failed to delete topic {topic}: {e}")
+# 
+#     futs = admin.create_topics([confluent_kafka.admin.NewTopic(topic, num_partitions=1, replication_factor=1) 
+#                          for topic in new_topics])
+#     for topic, future in futs.items():
+#         try:
+#             future.result()
+#             print(f"Created topic {topic}")
+#         except Exception as e:
+#             print(f"Failed to create topic {topic}: {e}")
+#             
+#     print(f"New topics:\n{new_topics}")
+#     print(f"Current topics:\n{pprint.pformat(admin.list_topics().topics)}")
+
 
 def main():
     producer_topic = rand_string("prod_topic_")
@@ -20,6 +47,12 @@ def main():
     producer = Prod()
     consumer = Cons()
 
+    # consumer can't subscribe to non-existing topic, so produce single message to create it
+    # and discard it from consumer
+    producer.produce(consumer_topic, amount=1, no_cb=True)
+    result = consumer.consume(consumer_topic, max_messages=1)
+    print(f"Created consumer topic and read message {result}")
+
     start_time = time.perf_counter()
     producer.produce(producer_topic)
     result = consumer.consume(consumer_topic)
@@ -30,7 +63,7 @@ def main():
     else:
         print("Failed to consume any messages")  
     
-    stream_query_proc.kill()
+    # TODO: stream_query_proc.kill()
 
 if __name__ == "__main__":
     main()
