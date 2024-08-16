b4ce95b2ab1ec1d835fbfe38437c00892599c032
carloronconi <carlo.ronconi@mail.polimi.it>
2024-08-16 11:46:25 +0200

    feat: change risingwave startup script to cleanup old sources, views and sinks

--- benchmark/start_risingwave_local.sh
+++ benchmark/start_risingwave_local.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # download official risingwave install script
-curl https://risingwave.com/sh | sh
-# to run risingwave, just do `./risingwave`
+if [ ! -f risingwave ]; then
+    curl https://risingwave.com/sh | sh
+fi
 
 # in case there is an error with libssl1.1, download and install it manually
 # https://stackoverflow.com/a/73603200
-# for debian, not sure what to do
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+# for sola, this wasn't required
+# wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+# sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+# to run risingwave, just do `./risingwave`
+
+# check if directory risingwave_state_dir exists: if it does, delete it to start fresh
+if [ -d risingwave_state_dir ]; then
+    rm -rf risingwave_state_dir
+fi
+
+mkdir risingwave_state_dir
+./risingwave --store-directory risingwave_state_dir
