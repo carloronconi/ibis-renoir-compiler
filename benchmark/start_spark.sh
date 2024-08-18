6e325441e7447e05191aee140de9d87ae963bcdb
carloronconi <carlo.ronconi@mail.polimi.it>
2024-08-18 16:57:05 +0200

    fix: update spark start script for sola1 compatibility

--- benchmark/start_spark.sh
+++ benchmark/start_spark.sh
@@ -11,8 +11,15 @@ if [ ! -d $dir ]; then
     tar -xzf $file
 fi
 
+# only for sola
+if [[ $(hostname) == "sola1" ]]; then
+    echo "sola1 detected, overriding default java to use java 11"
+    export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+    export PATH=$JAVA_HOME/bin:$PATH
+fi
+
 cd $dir
-./sbin/start-master.sh -h local
+./sbin/start-master.sh -h 127.0.0.1
 
 # https://issues.apache.org/jira/browse/SPARK-30978
 # Based on our experience, there is no scenario that necessarily requires 
@@ -20,4 +27,4 @@ cd $dir
 # A worker should book all the resources reserved to Spark on the host 
 # it is launched, then it can allocate those resources to one or more 
 # executors launched by this worker.
-./sbin/start-worker.sh spark://local:7077 
+./sbin/start-worker.sh spark://127.0.0.1:7077 
